35b0e0dcb3a77ee3d8a593dc0e9339d8d454ad9d
ANS <36783010+answebdev@users.noreply.github.com>
2021-03-15 21:22:36 -0700

    style Error Page

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
+import classes from '../styles/Error.module.css';
 
-function Error() {
+// GIF Source: https://giphy.com/stickers/retro-mixtape-cassette-XDutFIsipAQk1iAns2
+
+const Error = () => {
   return (
     <div>
       <Helmet>
         <title>Mixtape | Error</title>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css'
+        />
+        <style type='text/css'>{`
+        body {
+          font-family: 'Poppins', sans-serif;
+          letter-spacing: 1px;
+          background-color:#b2a996;
+        }
+        
+        .navbar {
+          display: none;
+        }
+    `}</style>
       </Helmet>
-      <h1>Error Page</h1>
+      <Container className={classes.CassetteContainer} fluid>
+        <div className={classes.Parent}>
+          <div className={classes.Child1}>
+            <h4>OOPS!</h4>
+            <h4>404 Page Not Found.</h4>
+          </div>
+          <div className={classes.Child2}>
+            <img
+              className={classes.Cassette}
+              src='https://media2.giphy.com/media/XDutFIsipAQk1iAns2/source.gif'
+              alt='Cassette'
+              title='Cassette'
+            />
+          </div>
+          <div className={classes.Child3}>
+            <Link className={classes.BackBtn} to='/artists'>
+              <i className='fas fa-arrow-left'></i> BACK TO SAFETY
+            </Link>
+          </div>
+        </div>
+      </Container>
     </div>
   );
-}
+};
 
 export default Error;
